4b493f15e18db0d8f9df8c36efe9d00bde00b1ed
Mateus Koppe <mateus.koppe@gmail.com>
2022-02-18 12:42:20 -0300

    Refactor keyword->grammar

--- src/compiler/grammar.clj
+++ src/compiler/grammar.clj
@@ -1,15 +1,16 @@
 (ns compiler.grammar)
 
 (defn keywork->grammar [token]
-  {:states (merge
-            (apply merge
-                   (map-indexed
-                    (fn [i non-terminal] {i {:productions {non-terminal [(inc i)]}
-                                             :is-final false}}) (str token)))
-            {(count string) {:productions []
-                             :is-final true}})
-   :token (keyword string)})
-
-(map #(vector %) (str :if))
-
-(count :str)
+  (let [string (str token)
+        last-state {(count string)
+                    {:productions []
+                     :is-final true}}
+        map->state (fn [i non-terminal]
+                     {i {:productions {non-terminal [(inc i)]}
+                         :is-final false}})]
+    {:states (merge (apply
+                     merge
+                     (map-indexed map->state
+                                  string))
+                    last-state)
+     :token (keyword string)}))
